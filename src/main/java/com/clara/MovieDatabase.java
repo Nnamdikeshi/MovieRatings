68fc9d34f7a20aaecb62a4f5c1c261c8cd73e418
Nc0614dn <Me@me.com>
2016-11-15 12:13:50 -0600

    Commit

--- src/main/java/com/clara/MovieDatabase.java
+++ src/main/java/com/clara/MovieDatabase.java
@@ -5,8 +5,8 @@ public class MovieDatabase {
 
     private static String DB_CONNECTION_URL = "jdbc:mysql://localhost:3306/";
     private static final String DB_NAME = "movies";
-    private static final String USER = "clara";
-    private static final String PASS = "mypass";   //todo change
+    private static final String USER = "root";
+    private static final String PASS = "itecitec";   //todo change
 
     //todo grant permissions to your user for this table, for example
     //grant create, select, insert, update, drop, delete on movie_reviews to 'clara'@'localhost'
